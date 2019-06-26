f02ae0a7d17ae9a36665accf6b5881de02f80717
unknown <maham@enablersinc.com>
2019-06-26 16:18:24 +0500

    fix merge conflicts

--- src/components/Onboarding/Workspace/WorkspaceContainer.tsx
+++ src/components/Onboarding/Workspace/WorkspaceContainer.tsx
@@ -7,18 +7,8 @@ import styles from "../styles.module.scss";
 const WorkspaceContainer: React.FC = () => {
   return (
     <div className="WorkspaceContainer">
-<<<<<<< HEAD
-      <span>
-        <InputField placeholder={"Workspace Name"} />
-        <span className={styles.textfield}>
-          <InputField placeholder={"Owner"} />
-        </span>
-      </span>
       <AddOrganizationDomain />
       <AddWorkspaceManagers />
-=======
-      <p>WorkspaceContainer</p>
->>>>>>> 742c95b87cf245af05daa92dea841fe808670f6a
     </div>
   );
 };
