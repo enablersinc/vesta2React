4e6587e6968daae120d226b8a2718665b8865698
mabbasi <maham@enablersinc.com>
2019-06-27 18:02:25 +0500

    update organziation components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.scss";
 import OnboardingContainer from './Onboarding/OnboardingContainer';
-import Permissions from './Onboarding/Organization/AddRolePermissions/Permissions'
-import AddWorkforceContainer from '../components/Onboarding/Organization/AddWorkforce/AddWorkforceContainer'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <AddWorkforceContainer/>
+      <OnboardingContainer/>
     </div>
   );
 };
