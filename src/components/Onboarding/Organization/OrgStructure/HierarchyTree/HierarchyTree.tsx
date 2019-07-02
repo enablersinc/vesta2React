b9863b552b967c8139719620a39241b9d64ae808
Jawad Farooqi <jawadfarooqi17@gmail.com>
2019-07-02 15:37:25 +0500

    complete hierarchy tree

--- src/components/Onboarding/Organization/OrgStructure/HierarchyTree/HierarchyTree.tsx
+++ src/components/Onboarding/Organization/OrgStructure/HierarchyTree/HierarchyTree.tsx
@@ -7,6 +7,7 @@ import {
   ExpansionPanelDetails
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import "./HierarchyTree.scss";
 
 type myProps = {
   hierarchyTree: Array<{
@@ -25,20 +26,25 @@ class HierarchyTree extends React.Component<myProps> {
     return (
       <div>
         {_data.map((obj, index) => {
-          <Box key={index} m={1}>
-            <ExpansionPanel defaultExpanded={index === 0}>
-              <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>{obj.name}</Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <HierarchyTree hierarchyTree={obj.children} />
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </Box>;
+          return (
+            <Box key={index} m={1} borderLeft={1}>
+              <ExpansionPanel defaultExpanded className={"expansion-panel"}>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <ExpandMoreIcon />
+                  <Typography>{obj.name}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  {obj.children !== undefined && obj.children.length > 0 && (
+                    <HierarchyTree hierarchyTree={obj.children} />
+                  )}
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Box>
+          );
         })}
       </div>
     );
